f849f29f98570f9d6b0290540fabc279a5e7cf1c
AlexCT <tresseltac@gmail.com>
2022-06-14 21:46:49 -0700

    Create files

---
+++ script.js
@@ -0,0 +1,12 @@
+// The Odin Project: Etch A Sketch
+// Alex Tresselt 6-14-22
+
+const container = document.getElementById("#container");
+
+// Draws the grid with the given number of squares per side.
+function drawGrid(squaresPerSide) {
+
+}
+
+// Initialize page with a 16x16 grid
+drawGrid(16);
